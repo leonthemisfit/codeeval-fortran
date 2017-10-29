cb20b20f85e35f2d3ce939030b839828f66faf6e
Leon <leonthemisfit@gmail.com>
2017-10-28 23:00:01 -0400

    Added challenge 25, Odd Numbers

---
+++ 025_odd_numbers.f90
@@ -0,0 +1,11 @@
+! https://www.codeeval.com/open_challenges/25/
+
+program odd_numbers
+  implicit none
+
+  integer(4) :: i
+
+  do i = 1, 99, 2
+    print "(i0)", i
+  end do
+end program odd_numbers
