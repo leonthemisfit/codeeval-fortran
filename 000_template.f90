a9fab7107813390d602919a7304cc19c2e313326
Leon <leonthemisfit@gmail.com>
2017-10-28 22:50:13 -0400

    Added challenge template

---
+++ 000_template.f90
@@ -0,0 +1,80 @@
+! URL
+
+program NAME
+  implicit none
+
+  integer(4), parameter :: STRING_LENGTH = 256
+  integer(4), parameter :: PATH_LENGTH = 128
+  integer(4), parameter :: INT_LENGTH = 4
+  integer(4), parameter :: FILE_UNIT = 99
+  character(*), parameter :: FMT_INT = "(i0)"
+  character(*), parameter :: FMT_STR = "(a)"
+  character(*), parameter :: FMT_RINT = "(i4)"
+  character, parameter :: EOF = char(0)
+  character, parameter :: DELIM = " "
+
+  character(STRING_LENGTH) :: line
+
+  call openfile()
+  do
+    line = readline()
+    select case (line)
+    case (EOF)
+      exit
+    case ("")
+      continue
+    case default
+      ! DO STUFF
+    end select
+  end do
+  close(FILE_UNIT)
+contains
+  subroutine openfile()
+    character(PATH_LENGTH) :: path
+
+    call getarg(1, path)
+    open(FILE_UNIT, file=trim(path))
+  end subroutine openfile
+
+  function readline() result(line)
+    character(STRING_LENGTH) :: line
+    integer(1) :: io
+
+    read(FILE_UNIT, FMT_STR, iostat=io) line
+    if (io > 0) then
+      print FMT_STR, "ERR: Read Failed"
+    else if (io < 0) then
+      line = EOF
+    end if
+  end function readline
+
+  ! subroutine popint(first, rest)
+  !   integer(INT_LENGTH), intent(out) :: first
+  !   character(STRING_LENGTH), intent(inout) :: rest
+  !   integer(2) :: index
+  !
+  !   index = scan(trim(rest), DELIM)
+  !   if (index > 0) then
+  !     read(rest(1:index - 1), FMT_RINT) first
+  !     rest = rest(index + 1:)
+  !   else
+  !     read(rest, FMT_RINT) first
+  !     rest = ""
+  !   end if
+  ! end subroutine popint
+  !
+  ! subroutine popstring(first, rest)
+  !   character(STRING_LENGTH), intent(out) :: first
+  !   character(STRING_LENGTH), intent(inout) :: rest
+  !   integer(2) :: index
+  !
+  !   index = scan(trim(rest), DELIM)
+  !   if (index > 0) then
+  !     first = rest(1:index - 1)
+  !     rest = rest(index + 1:)
+  !   else
+  !     first = rest
+  !     rest = ""
+  !   end if
+  ! end subroutine popstring
+end program NAME
